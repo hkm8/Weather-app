487f1a47a14b4524da3af2083cfe7e62cf1592d0
hkm8 <adam.mikut@gmail.com>
2024-04-11 20:52:15 +0200

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Adam Mikut on 11/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
